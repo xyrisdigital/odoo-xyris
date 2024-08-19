4096c8c085c3ea3d8c3f28e3bc2a84ee65dbd95d
Xyris <ahmed.emara@xyrisdigital.com>
2024-08-19 15:25:20 +0300

    contract

--- my_hr/models/contracts.py
+++ my_hr/models/contracts.py
@@ -1,4 +1,6 @@
 from odoo import models, api, fields
+from odoo.exceptions import ValidationError, UserError
+from datetime import timedelta, datetime, time
 
 def calculate_tax_value(AG3):
     if AG3 <= 40000:
@@ -39,6 +41,73 @@ class mycontracts(models.Model):
     shohdaa = fields.Float(string="Elshohdaa", compute="_calculate_shohdaa")
 
     day_amount = fields.Float(string="Deduced Days/Day")
+
+    preparation_type = fields.Boolean(string="Preparation Period", default=False)
+    contract_type = fields.Boolean(string="Contract Period", default=False)
+
+
+
+    # @api.onchange('preparation_type')
+    @api.model
+    def get_date_avarage(self):
+        get_data_contract = self.env['hr.contract'].search_read([('state', '=', 'open')])
+        get_length = len(get_data_contract)
+        get_data = []
+        get_data_id = []
+        get_data_check = []
+        get_data_check_contract = []
+        get_date_now = datetime.now().date()
+        for rec in range(get_length):
+            get_data.append((get_data_contract[rec]['date_start'] + timedelta(days=60)))
+            get_data_id.append(get_data_contract[rec]['id'])
+            get_data_check.append(get_data_contract[rec]['preparation_type'])
+            get_data_check_contract.append(get_data_contract[rec]['contract_type'])
+            if get_data[rec] <= get_date_now and get_data_check[rec] == True:
+                self.env['mail.activity'].create({
+                'res_id': get_data_id[rec],
+                'res_model_id': self.env['ir.model'].search([('model', '=', 'hr.contract')]).id,
+                'user_id': 61,
+                'summary': 'Preparation Period',
+                'note': 'Preparation Period Will Expire',
+                'activity_type_id': 4,
+                'date_deadline': datetime.now().today(),
+                # 'date_deadline': date_deadline,
+            })
+            
+        self.get_date_avarage_2()
+                
+
+
+    @api.model
+    def get_date_avarage_2(self):
+        # commit
+        get_data_contract = self.env['hr.contract'].search_read([('state', '=', 'open')])
+        get_length = len(get_data_contract)
+        get_data = []
+        get_data_id = []
+        get_data_check = []
+        get_data_check_contract = []
+        get_date_now = datetime.now().date()
+        for rec in range(get_length):
+            # if get_data_contract[rec]['date_end'] != False:
+            # get_data.append((get_data_contract[rec]['state']))
+            get_data.append((get_data_contract[rec]['date_start'] + timedelta(days=305)))
+            # raise UserError('dataaaaaaaaaaa ' + str(get_data))
+            get_data_id.append(get_data_contract[rec]['id'])
+            # get_data_check.append(get_data_contract[rec]['preparation_type'])
+            get_data_check_contract.append(get_data_contract[rec]['contract_type'])
+            
+            if get_data[rec] <= get_date_now and get_data_check_contract[rec] == True:
+                self.env['mail.activity'].create({
+                'res_id': get_data_id[rec],
+                'res_model_id': self.env['ir.model'].search([('model', '=', 'hr.contract')]).id,
+                'user_id': 61,
+                'summary': 'Contract Period',
+                'note': 'Contract Period Will Expire',
+                'activity_type_id': 4,
+                'date_deadline': datetime.now().today(),
+                # 'date_deadline': date_deadline,
+            })    
 
 
     @api.depends('wage')
