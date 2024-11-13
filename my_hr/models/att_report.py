3e3dbf9ce249f0b3944204c78ea1fa9c48f6878e
root <root@vmi2267388.contaboserver.net>
2024-11-13 02:17:26 -0600

    .

--- my_hr/models/att_report.py
+++ my_hr/models/att_report.py
@@ -137,13 +137,58 @@ class Att_Report(models.Model):
 
 
     -- Percentage calculation using inline req_hours calculation with FLOOR applied
-    FLOOR((EXTRACT(EPOCH FROM (
-        COALESCE((SELECT SUM(COALESCE(worked_hours, 0)) * interval '1 hour'
+   (Case When 
+	((((SELECT 
+        count(*) 
+    	FROM 
+        generate_series(CAST('""" + str(start_date) + """' AS DATE)::date, CAST('""" + str(end_date) + """' AS DATE)::date, '1 day'::interval) AS date
+    	WHERE 
+        DATE_PART('dow', date) NOT IN (5, 6))) * ( case when employee_id in (7,11) then interval '6:45 hour' else  interval '7:45 hour' end)) 
+		-
+    	(COALESCE(((WITH leave_dates AS (
+    SELECT 
+        employee_id,
+        number_of_days,
+        generate_series(request_date_from::timestamp, request_date_to::timestamp, '1 day')::date AS leave_date
+    FROM 
+        hr_leave  ) 
+		SELECT 
+		sum(case when number_of_days >= 1 then 1 else number_of_days end) as real_days 
+		FROM leave_dates 
+		WHERE leave_date 
+		BETWEEN CAST('""" + str(start_date) + """' AS DATE) AND  CAST('""" + str(end_date) + """' AS DATE) AND leave_dates.employee_id = a.employee_id
+		GROUP BY employee_id) * ( case when employee_id in (7,11) then interval '6:45 hour' else  interval '7:45 hour' end)
+			),interval '0 hours'))		
+			-
+			(SELECT 
+    ((COALESCE((CASE 
+                WHEN Date_start - CAST('""" + str(start_date) + """' AS DATE) > 0 AND Date_start - CAST('""" + str(start_date) + """' AS DATE) < 30 
+                THEN Date_start - CAST('""" + str(start_date) + """' AS DATE)
+                ELSE 0 
+              END), 0) 
+     - 
+     COALESCE((CASE 
+                WHEN CAST('""" + str(end_date) + """' AS DATE) - date_end > 0 AND CAST('""" + str(end_date) + """' AS DATE) - date_end < 30 
+                THEN CAST('""" + str(end_date) + """' AS DATE) - date_end 
+                ELSE 0 
+              END), 0)) *
+			  ( case when employee_id in (7,11) then interval '6:45 hour' else  interval '7:45 hour' end)) AS total_hours
+		FROM hr_contract where hr_contract.employee_id = a.employee_id )
+		) = 0 * interval '0 hours' then 100
+
+		else
+
+   
+   FLOOR((EXTRACT
+
+	
+	(EPOCH FROM (
+        COALESCE((SELECT SUM(worked_hours) * interval '1 hour'
                   FROM hr_attendance b 
                   WHERE a.employee_id = b.employee_id 
                     AND remote = TRUE 
                     AND check_in BETWEEN CAST('""" + str(start_date) + """' AS DATE) AND CAST('""" + str(end_date) + """' AS DATE)), interval '0 hours') + 
-        COALESCE((SELECT SUM(COALESCE(worked_hours, 0)) * interval '1 hour'
+        COALESCE((SELECT SUM(worked_hours) * interval '1 hour'
                   FROM hr_attendance b 
                   WHERE a.employee_id = b.employee_id 
                     AND remote = FALSE 
@@ -152,46 +197,51 @@ class Att_Report(models.Model):
                                FROM hr_leave 
                                WHERE holiday_status_id = 11 
                                  AND hr_leave.employee_id = a.employee_id 
-                                 AND hr_leave.request_date_from BETWEEN '2024-10-01' AND '2024-10-30'
+                                 AND hr_leave.request_date_from BETWEEN CAST('""" + str(start_date) + """' AS DATE) AND CAST('""" + str(end_date) + """' AS DATE)
                                  AND hr_leave.state = 'validate'
                                LIMIT 1), interval '0 hours')), interval '0 hours')
-    )) / EXTRACT(EPOCH FROM ((((SELECT 
-        count(*) - """ + str(public_days) + """ 
+    ))
+	
+	
+	
+	/ EXTRACT(EPOCH FROM 
+	((((SELECT 
+        count(*) 
     	FROM 
         generate_series(CAST('""" + str(start_date) + """' AS DATE)::date, CAST('""" + str(end_date) + """' AS DATE)::date, '1 day'::interval) AS date
     	WHERE 
-        DATE_PART('dow', date) NOT IN (5, 6))) * ( case when employee_id in (7,11) then interval '6:45 hour' else  interval '7:45 hour' end)) -
-    	(
-    	COALESCE(
-    	    ((WITH leave_dates AS 
-			(
+        DATE_PART('dow', date) NOT IN (5, 6))) * ( case when employee_id in (7,11) then interval '6:45 hour' else  interval '7:45 hour' end)) 
+		-
+    	(COALESCE(((WITH leave_dates AS (
     SELECT 
         employee_id,
         number_of_days,
         generate_series(request_date_from::timestamp, request_date_to::timestamp, '1 day')::date AS leave_date
     FROM 
-        hr_leave  
-		) 
+        hr_leave  ) 
 		SELECT 
 		sum(case when number_of_days >= 1 then 1 else number_of_days end) as real_days 
 		FROM leave_dates 
 		WHERE leave_date 
 		BETWEEN CAST('""" + str(start_date) + """' AS DATE) AND CAST('""" + str(end_date) + """' AS DATE) AND leave_dates.employee_id = a.employee_id
 		GROUP BY employee_id) * ( case when employee_id in (7,11) then interval '6:45 hour' else  interval '7:45 hour' end)
-			),interval '0 hours'))-
+			),interval '0 hours'))		
+			-
 			(SELECT 
     ((COALESCE((CASE 
                 WHEN Date_start - CAST('""" + str(start_date) + """' AS DATE) > 0 AND Date_start - CAST('""" + str(start_date) + """' AS DATE) < 30 
-                THEN Date_start - CAST('""" + str(start_date) + """' AS DATE) 
+                THEN Date_start - CAST('""" + str(start_date) + """' AS DATE)
                 ELSE 0 
               END), 0) 
-     + 
+     - 
      COALESCE((CASE 
                 WHEN CAST('""" + str(end_date) + """' AS DATE) - date_end > 0 AND CAST('""" + str(end_date) + """' AS DATE) - date_end < 30 
                 THEN CAST('""" + str(end_date) + """' AS DATE) - date_end 
                 ELSE 0 
-              END), 0)) * ( case when employee_id in (7,11) then interval '6:45 hour' else  interval '7:45 hour' end)) AS total_hours
-		FROM hr_contract where hr_contract.employee_id = a.employee_id )))) * 100) AS percentage
+              END), 0)) *
+			  ( case when employee_id in (7,11) then interval '6:45 hour' else  interval '7:45 hour' end)) AS total_hours
+		FROM hr_contract where hr_contract.employee_id = a.employee_id )
+		))) * 100)end) AS percentage
 
 FROM hr_attendance a 
 WHERE check_in BETWEEN CAST('""" + str(start_date) + """' AS DATE) AND CAST('""" + str(end_date) + """' AS DATE) and Employee_id in (select Employee_id From hr_contract where state = 'open' )
@@ -210,37 +260,41 @@ GROUP BY employee_id;
 
         for rec in range(get_length):
 
-            # Calculate total hours - Required Hours
-            total_hours = result[rec][1].days * 24 + result[rec][1].seconds // 3600
-            remaining_seconds = result[rec][1].seconds % 3600
-            minutes = remaining_seconds // 60
-            seconds = remaining_seconds % 60
+            # raise UserError('dataaaaaaaaaaaaaaa ' + str(result[rec][1].days))
+            if result[rec][1] != None:
+                # Calculate total hours - Required Hours
+                total_hours = result[rec][1].days * 24 + result[rec][1].seconds // 3600 if result[rec][1].days != 0 else result[rec][1].seconds // 3600
+                remaining_seconds = result[rec][1].seconds % 3600
+                minutes = remaining_seconds // 60
+                seconds = remaining_seconds % 60
 
+            if result[rec][2] != None:
+                # Calculate total hours - Remote Hours
+                total_hours_remote = result[rec][2].days * 24 + result[rec][2].seconds // 3600 if result[rec][2].days != 0 else result[rec][2].seconds // 3600
+                remaining_seconds_remote = result[rec][2].seconds % 3600
+                minutes_remote = remaining_seconds_remote // 60
+                seconds_remote = remaining_seconds_remote % 60
 
-            # Calculate total hours - Remote Hours
-            total_hours_remote = result[rec][2].days * 24 + result[rec][2].seconds // 3600
-            remaining_seconds_remote = result[rec][2].seconds % 3600
-            minutes_remote = remaining_seconds_remote // 60
-            seconds_remote = remaining_seconds_remote % 60
+            if result[rec][3] != None:
+                # Calculate total hours - Office Hours
+                total_hours_office = result[rec][3].days * 24 + result[rec][3].seconds // 3600 if result[rec][3].days != 0 else result[rec][3].seconds // 3600
+                #  total_hours_office = result[rec][3].days * 24 + result[rec][3].seconds // 3600 if result[rec][3].days != 0 else result[rec][3].seconds // 3600
+                remaining_seconds_office = result[rec][3].seconds % 3600
+                minutes_office = remaining_seconds_office // 60
+                seconds_office = remaining_seconds_office % 60
 
-
-            # Calculate total hours - Office Hours
-            total_hours_office = result[rec][3].days * 24 + result[rec][3].seconds // 3600
-            remaining_seconds_office = result[rec][3].seconds % 3600
-            minutes_office = remaining_seconds_office // 60
-            seconds_office = remaining_seconds_office % 60
-
-            # Calculate total hours - Overtime Hours
-            total_hours_overtime = result[rec][5].days * 24 + result[rec][5].seconds // 3600
-            remaining_seconds_overtime = result[rec][5].seconds % 3600
-            minutes_overtime = remaining_seconds_overtime // 60
-            seconds_overtime = remaining_seconds_overtime % 60            
-
-            # Calculate total hours - Final Total
-            total_hours_all = result[rec][6].days * 24 + result[rec][6].seconds // 3600
-            remaining_seconds_all = result[rec][6].seconds % 3600
-            minutes_all = remaining_seconds_all // 60
-            seconds_all = remaining_seconds_all % 60 
+            if result[rec][5] != None:
+                # Calculate total hours - Overtime Hours
+                total_hours_overtime = result[rec][5].days * 24 + result[rec][5].seconds // 3600 if result[rec][5].days != 0 else result[rec][5].seconds // 3600
+                remaining_seconds_overtime = result[rec][5].seconds % 3600
+                minutes_overtime = remaining_seconds_overtime // 60
+                seconds_overtime = remaining_seconds_overtime % 60            
+            if result[rec][6] != None:
+                # Calculate total hours - Final Total
+                total_hours_all = result[rec][6].days * 24 + result[rec][6].seconds // 3600 if result[rec][6].days != 0 else result[rec][6].seconds // 3600
+                remaining_seconds_all = result[rec][6].seconds % 3600
+                minutes_all = remaining_seconds_all // 60
+                seconds_all = remaining_seconds_all % 60 
 
 
 
--- my_hr/wizard/attendance_report.py
+++ my_hr/wizard/attendance_report.py
@@ -15,7 +15,28 @@ class ReportWizard(models.TransientModel):
         # if self.start_date >= self.end_date:
         #     raise models.ValidationError("Start date must be earlier than the end date.")
 
+        self.env['att.report'].sudo().search([]).unlink()
+
         # Run your SQL query
         get_function = self.env['att.report'].sudo().search([])
 
         get_function.calculate_report(self.public_days, self.start_date, self.end_date)
+
+        # return {
+        #     'type': 'ir.actions.client',
+        #     'tag': 'reload',
+        # }
+
+        # return {
+        #     'type': 'ir.actions.client',
+        #     'tag': 'reload',
+        # }
+
+        return {
+            'type': 'ir.actions.act_url',
+            'url': 'https://erp.xyrisdigital.com/web#action=611&model=att.report&view_type=list&cids=1&menu_id=401',
+            # 'target': 'self',  # Options: 'self' for the same window, 'new' for a new tab
+            'tag': 'reload',
+        }
+
+        
